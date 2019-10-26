f42f08efaadcc24bec04d1896f4a04bae268f9ab
Juan Dominguez <juan.dominguez.developer@gmail.com>
2019-10-25 23:46:42 -0700

    add API Framework and user model

--- Activ5Extension/AppDelegate.swift
+++ Activ5Extension/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Activ5Device
+import ActivSync
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         A5DeviceManager.initializeDeviceManager()
+        if let url = URL(string: "https://sdhacks.activ5.com") {
+            ActivSync.configure(url: url)
+        }
         return true
     }
 
