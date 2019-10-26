34f6637d24e2461eeb156a94d4374fc6a411227a
Juan Dominguez <juan.dominguez.developer@gmail.com>
2019-10-25 22:06:16 -0700

    Initial Commit

---
+++ Activ5Extension/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Activ5Extension
+//
+//  Created by Juan Dominguez on 10/25/19.
+//  Copyright © 2019 VBC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Activ5ExtensionTests/Activ5ExtensionTests.swift
@@ -0,0 +1,34 @@
+//
+//  Activ5ExtensionTests.swift
+//  Activ5ExtensionTests
+//
+//  Created by Juan Dominguez on 10/25/19.
+//  Copyright © 2019 VBC. All rights reserved.
+//
+
+import XCTest
+@testable import Activ5Extension
+
+class Activ5ExtensionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
